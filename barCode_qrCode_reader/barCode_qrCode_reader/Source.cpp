bdde3392a119bb6c82cc1dbb40d5f3f1ed2b1ab1
unknown <elon.gilad9@gmail.com>
2024-12-06 16:24:29 +0200

    minimal changes, not rellevant

--- barCode_qrCode_reader/barCode_qrCode_reader/Source.cpp
+++ barCode_qrCode_reader/barCode_qrCode_reader/Source.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    cv::Mat img = cv::imread("C:\\Users\\test0\\OneDrive\\שולחן העבודה\\project\\barCode-and-qrCode-reader\\tests\\1014500044744.png");
+    cv::Mat img = cv::imread("C:\\Users\\eylon\\Desktop\\projects\\barCode-and-qrCode-reader\\tests\\3232229999929.png");
     cv::namedWindow("First OpenCV Application", cv::WINDOW_AUTOSIZE);
     cv::imshow("First OpenCV Application", img);
     cv::moveWindow("First OpenCV Application", 0, 45);
